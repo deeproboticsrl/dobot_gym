d4d03296636c1e59ae0fafb0c45f33eb7dd48159
JoyChopra1298 <joy.chopra2@gmail.com>
2019-03-14 19:38:45 +0530

    3d plot minor refactor

--- dobot_gym/envs/sim/single_point/reward_plot.py
+++ dobot_gym/envs/sim/single_point/reward_plot.py
@@ -4,29 +4,29 @@ from mpl_toolkits import mplot3d
 
 s0 = np.array([0, 0])
 g = np.array([1, 0])
+midpoint = (s0 + g) / 2
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-num_points = 300
+num_points = 200
 num_contours = 100
-x = np.linspace(-1, 2, num_points)
-y = np.linspace(-1, 1, num_points)
+x = np.linspace(midpoint[0] - 1, midpoint[0] + 1, num_points)
+y = np.linspace(midpoint[1] - 1, midpoint[1] + 1, num_points)
 
 scale_factor_s0 = 1
 scale_factor_g = 1
 
 X, Y = np.meshgrid(x, y)
 
-Z = scale_factor_s0 * (X ** 2 + Y ** 2) + scale_factor_g * ((X - 1) ** 2 + Y ** 2)
+Z = scale_factor_s0 * ((X - s0[0]) ** 2 + (Y - s0[1]) ** 2) + scale_factor_g * ((X - g[0]) ** 2 + (Y - g[1]) ** 2)
 
 # Contours are elliptical. Scale factor only shifts the center of ellipses
 
 ax.contour3D(X, Y, Z, num_contours)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
-ax.set_zlabel('z')
+ax.set_zlabel('Reward')
 
 plt.show()
 # reward_function = np.abs(s0 - s) ** 2 + np.abs(s - g) ** 2 where s = [x,y]
-
